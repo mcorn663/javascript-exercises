e1c1e93e50e5f670c477996cfc6dba7ae93c9172
Megan Cornell <58535837+mcorn663@users.noreply.github.com>
2020-06-23 06:33:59 -0400

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let a = prompt('Choose your first number?);
+    let b = prompt('Choose your second number?);
+                   
+   for (let i = a, i < b, i ++) {
+     console.log('The totoal is'+ i);
 }
 
 module.exports = sumAll
