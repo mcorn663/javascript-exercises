fa5abab12cc1ded4cc28f0f63a87b98092d8a0e4
Megan Cornell <58535837+mcorn663@users.noreply.github.com>
2020-06-22 20:29:58 -0400

    Update reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  var splitString = str.split(' ');
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArry.join(' ');
+  return joinArray;
 }
 
+                                  
 module.exports = reverseString
